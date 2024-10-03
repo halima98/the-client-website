4b05df93b99794ea6f36f2c2eeb2d7c34f85569a
Halima Dinle <26465999+halima98@users.noreply.github.com>
2024-10-03 12:37:47 +0200

    Blob animatie #4

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,5 @@ var cursor = document.querySelector('.blob');
 document.addEventListener('mousemove', function(event) {
   var x = event.clientX;
   var y = event.clientY;
-
   cursor.style.transform = 'translate3d(' + (x - 50) + 'px, ' + (y - 50) + 'px, 0)';
 });
