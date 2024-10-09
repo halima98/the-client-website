652789439bd79c374675f3812149f23e8b016f0a
Halima Dinle <26465999+halima98@users.noreply.github.com>
2024-10-09 12:20:09 +0200

    #10

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
-var cursor = document.querySelector('.blob');
+const cursor = document.querySelector('.blob');
 document.addEventListener('mousemove', function(event) {
-  var x = event.clientX;
-  var y = event.clientY;
+ const x = event.clientX;
+ const y = event.clientY;
   cursor.style.transform = 'translate3d(' + (x - 50) + 'px, ' + (y - 50) + 'px, 0)';
 });
